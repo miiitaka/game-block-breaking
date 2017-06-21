37dea1cbceb5a24293c1182b959bc38c387f0bf3
Kazuya Takami <foundationmeister@outlook.com>
2017-06-21 20:54:33 +0900

    style.css added.

--- block-breaking.js
+++ block-breaking.js
@@ -6,29 +6,27 @@ var svgns = 'http://www.w3.org/2000/svg';
 stage.setAttribute('xmlns', svgns);
 
 function Block() {
+  this.x = 0;
+  this.y = 0;
+  this.w = 0;
+  this.h = 0;
   this.initialize.apply(this, arguments);
 }
 
-Block.prototype = {
-  x: 0,
-  y: 0,
-  w: 0,
-  h: 0,
-  life: 1,
-  initialize: function(option) {
-    this.x = option.x;
-    this.y = option.y;
-    this.w = option.w;
-    this.h = option.h;
-  },
-  draw: function() {
-    var rect = document.createElementNS(svgns, 'rect');
-    rect.setAttribute('x', String(this.x));
-    rect.setAttribute('y', String(this.y));
-    rect.setAttribute('width', String(this.w));
-    rect.setAttribute('height', String(this.h));
-    stage.appendChild(rect);
-  }
+Block.prototype.initialize = function(option) {
+  this.x = option.x;
+  this.y = option.y;
+  this.w = option.w;
+  this.h = option.h;
+};
+
+Block.prototype.draw = function() {
+  var rect = document.createElementNS(svgns, 'rect');
+  rect.setAttribute('x', String(this.x));
+  rect.setAttribute('y', String(this.y));
+  rect.setAttribute('width', String(this.w));
+  rect.setAttribute('height', String(this.h));
+  stage.appendChild(rect);
 };
 
 var block = new Block({
